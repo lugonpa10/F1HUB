29c35e4168805f8e4987aafc060c764e934b580d
lugonpa10 <lugonpa10@gmail.com>
2026-01-14 18:30:25 +0100

    changes

--- Movil/F1HUB/app/build.gradle.kts
+++ Movil/F1HUB/app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.f1hub"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.f1hub"
--- Movil/F1HUB/app/src/main/java/com/example/f1hub/Registro.java
+++ Movil/F1HUB/app/src/main/java/com/example/f1hub/Registro.java
@@ -1,6 +1,11 @@
 package com.example.f1hub;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +13,39 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Registro extends AppCompatActivity {
 
+
+public class Registro extends AppCompatActivity {
+    boolean muestra=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_registro);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        final String[] provincias=getResources().getStringArray(R.array.generos);
+        Spinner spinner=findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, provincias);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view,
+                                       int i, long l) {
+// Se usa la variable booleana para impedir que muestre el resultado
+// seleccionado por defecto cuando se accede al activity. Solo mostramos
+// cuando seleccionamos una opción.
+                if (!muestra) muestra=true;
+                else {
+                    Toast.makeText(getApplicationContext(), "Has selecionado: " +
+                                    adapterView.getItemAtPosition(i).toString(),
+                            Toast.LENGTH_SHORT).show();
+                    setTitle(adapterView.getItemAtPosition(i).toString());
+                }
+            }
+            // Se ejecuta cuando se pulsa fuera del Spinner: cuandono se selecciona nada
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+
         });
     }
 }
