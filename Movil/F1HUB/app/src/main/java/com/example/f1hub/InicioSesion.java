6846daf7079d947236aa77622f84de8cc8327029
lugonpa10 <lugonpa10@gmail.com>
2026-01-15 21:36:44 +0100

    changes

--- Movil/F1HUB/app/src/main/java/com/example/f1hub/InicioSesion.java
+++ Movil/F1HUB/app/src/main/java/com/example/f1hub/InicioSesion.java
@@ -1,6 +1,17 @@
 package com.example.f1hub;
 
+import android.content.Intent;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +20,15 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class InicioSesion extends AppCompatActivity {
+
+    ImageButton btnVerContraseña;
+    EditText etPassword;
+    EditText etUsuario;
+
+    Button btnIniciarSesion;
+
+    TextView txtCrearCuenta;
+    boolean passVisible = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +40,56 @@ public class InicioSesion extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        btnVerContraseña = findViewById(R.id.btnVerContraseña);
+        etPassword = findViewById(R.id.etContraseña);
+        etUsuario = findViewById(R.id.etUsuario);
+        btnIniciarSesion = findViewById(R.id.btnIniciarSesion);
+        txtCrearCuenta = findViewById(R.id.txtCrearCuenta);
+        txtCrearCuenta.setPaintFlags(txtCrearCuenta.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+        btnVerContraseña.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (passVisible) {
+                    etPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    passVisible = false;
+                    btnVerContraseña.setImageResource(R.drawable.ojocerrado);
+                } else {
+                    etPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    passVisible = true;
+                    btnVerContraseña.setImageResource(R.drawable.ojoabierto);
+                }
+
+            }
+
+
+        });
+
+
+        btnIniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String usuario = etUsuario.getText().toString().trim();
+                String contrasenha = etPassword.getText().toString().trim();
+
+                if (usuario.equals("lugonpa") && contrasenha.equals("1234")) {
+                    Intent intent = new Intent(InicioSesion.this, MainActivity.class);
+                    startActivity(intent);
+                }else if (!usuario.equals("lugonpa")){
+                    Toast.makeText(InicioSesion.this, "El usuario no es correcto", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(InicioSesion.this, "La contraseña no es correcta", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        txtCrearCuenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(InicioSesion.this, Registro.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
