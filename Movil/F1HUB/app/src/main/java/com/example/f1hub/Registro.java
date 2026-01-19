102839ecb0538221bd3be99981f2a6a4a5785a5c
lugonpa10 <lugonpa10@gmail.com>
2026-01-19 18:29:11 +0100

    changes

--- Movil/F1HUB/app/src/main/java/com/example/f1hub/Registro.java
+++ Movil/F1HUB/app/src/main/java/com/example/f1hub/Registro.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,12 +21,20 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Registro extends AppCompatActivity {
     boolean muestra=false;
+    boolean datosCorrectos = true;
     Button btnSiguiente;
+    EditText etNombre,etApellidos,etFechaNacimiento,etCorreo,etNombreUsuario;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
         btnSiguiente = findViewById(R.id.btnSiguiente);
+        etNombre = findViewById(R.id.etNombre);
+        etApellidos = findViewById(R.id.etApellidos);
+        etFechaNacimiento = findViewById(R.id.etFecha);
+        etCorreo = findViewById(R.id.etCorreo);
+        etNombreUsuario = findViewById(R.id.etNombreUsuario);
 
         final String[] provincias=getResources().getStringArray(R.array.generos);
         Spinner spinner=findViewById(R.id.spinner);
@@ -64,8 +73,22 @@ public class Registro extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(Registro.this,RegistroPassword.class);
-                startActivity(intent);
+                String nombre = etNombre.getText().toString();
+                String apellidos = etApellidos.getText().toString();
+                String fechaNacimiento = etFechaNacimiento.getText().toString();
+                String correo = etCorreo.getText().toString();
+                String nombreUsuario = etNombreUsuario.getText().toString();
+
+                if (nombre.isEmpty() || apellidos.isEmpty()  || fechaNacimiento.isEmpty() ||  correo.isEmpty() ||  nombreUsuario.isEmpty()){
+                    Toast.makeText(Registro.this,"Todos los campos son obligatorios",Toast.LENGTH_SHORT).show();
+
+                } else if ( nombre.equals("Nombre") || apellidos.equals("Apellidos") || fechaNacimiento.equals("Fecha") || correo.equals("Correo") || nombreUsuario.equals("Mote")){
+                    Toast.makeText(Registro.this,"Datos no validos",Toast.LENGTH_SHORT).show();
+                }else {
+                    Intent intent = new Intent(Registro.this,RegistroPassword.class);
+                    startActivity(intent);
+                }
+
             }
         });
 
--- Movil/F1HUB/app/src/main/java/com/example/f1hub/RegistroPassword.java
+++ Movil/F1HUB/app/src/main/java/com/example/f1hub/RegistroPassword.java
@@ -1,6 +1,9 @@
 package com.example.f1hub;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -14,37 +17,52 @@ public class RegistroPassword extends AppCompatActivity {
     EditText etContrasenha;
     EditText etRepetirContrasenha;
 
+    Button btnConfirmar;
+
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+
+        setContentView(R.layout.activity_registro_password);
         etContrasenha = findViewById(R.id.etPassword);
         etRepetirContrasenha = findViewById(R.id.etRepetirPassword);
-        setContentView(R.layout.activity_registro_password);
+        btnConfirmar = findViewById(R.id.btnConfirmar);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        String contrasenha = etContrasenha.getText().toString();
-        String repetirContrasenha = etRepetirContrasenha.getText().toString();
-        if (contrasenhaValida(contrasenha)) {
-            Toast.makeText(this,"Contraseña válida",Toast.LENGTH_SHORT).show();
-        } else if (contrasenha != repetirContrasenha) {
-            Toast.makeText(this,"Las contraseñas no coinciden",Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(this,
-                    "La contraseña debe tener:\n" +
-                            "• 10 caracteres\n" +
-                            "• 1 mayúscula\n" +
-                            "• 1 minúscula\n" +
-                            "• 1 número\n" +
-                            "• 1 carácter especial",
-                    Toast.LENGTH_LONG).show();
-        }
+
+
+        btnConfirmar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String contrasenha = etContrasenha.getText().toString();
+                String repetirContrasenha = etRepetirContrasenha.getText().toString();
+                if (contrasenhaValida(contrasenha)) {
+                    Toast.makeText(RegistroPassword.this, "Contraseña válida", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(RegistroPassword.this,MainActivity.class);
+                    startActivity(intent);
+                } else if (!contrasenha.equals(repetirContrasenha)) {
+                    Toast.makeText(RegistroPassword.this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(RegistroPassword.this,
+                            "La contraseña debe tener:\n" +
+                                    "• 10 caracteres\n" +
+                                    "• 1 mayúscula\n" +
+                                    "• 1 minúscula\n" +
+                                    "• 1 número\n" +
+                                    "• 1 carácter especial",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
+
+
 
     public static boolean contrasenhaValida(String contrasenha) {
         String requisitos = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&#._-])[A-Za-z\\d@$!%*?&#._-]{10,}$";
