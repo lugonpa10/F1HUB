d68ccf748a18e1cbbc625d802dfd3ca781cbbf3d
lugonpa10 <lugonpa10@gmail.com>
2026-02-02 10:20:20 +0100

    ajuste de las rutas en las imagenes

--- Web/public/js/slider.js
+++ Web/public/js/slider.js
@@ -3,21 +3,20 @@ document.addEventListener('DOMContentLoaded', () => {
     const rightPanel = document.getElementById('slider-right');
 
 
-   const images = [
-        'http://localhost:8000/imagenes/ferrari.jpg',
-        'http://localhost:8000/imagenes/alonsoPodio.jpg',
-        'http://localhost:8000/imagenes/max.jpg',
-        'http://localhost:8000/imagenes/senna.jpg',
-
+    const images = [
+        '../public/imagenes/ferrari.jpg',
+        '../public/imagenes/alonsoPodio.jpg',
+        '../public/imagenes/max.jpg',
+        '../public/imagenes/senna.jpg'
 
     ];
 
     let currentIndex = 0;
 
     function changeImages() {
-        
+
         const leftIndex = currentIndex % images.length;
-        const rightIndex = (currentIndex + 2) % images.length; 
+        const rightIndex = (currentIndex + 2) % images.length;
 
         leftPanel.style.backgroundImage = `url('${images[leftIndex]}')`;
         rightPanel.style.backgroundImage = `url('${images[rightIndex]}')`;
@@ -25,7 +24,7 @@ document.addEventListener('DOMContentLoaded', () => {
         currentIndex++;
     }
 
-   
+
     changeImages();
 
 
